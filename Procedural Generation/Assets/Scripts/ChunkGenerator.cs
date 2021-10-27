eecb2e77e24fdffe1eaa9fd021c78f4fcaa340c6
Nfleig <65872728+Nfleig@users.noreply.github.com>
2021-10-27 19:44:02 -0400

    Fixed chunk alignment issues. Adjusted worldgen algorithm in many ways

--- Procedural Generation/Assets/Scripts/ChunkGenerator.cs
+++ Procedural Generation/Assets/Scripts/ChunkGenerator.cs
@@ -9,11 +9,6 @@ public class ChunkGenerator : MonoBehaviour
 
     Vector3[] vertices;
     int[] triangles;
-    public int xSize;
-    public int zSize;
-    public float scale;
-    public float depth;
-    public int resolution;
     public float xOffset;
     public float yOffset;
     public int octaves;
@@ -21,17 +16,13 @@ public class ChunkGenerator : MonoBehaviour
     public float lacunarity;
     private float[,] heightMap;
     // Start is called before the first frame update
-    void Update()
-    {
-        GenerateTerrain();
-        
-    }
+    
 
-    public void GenerateTerrain()
+    public void GenerateTerrain(int xSize, int ySize, float scale, float depth, AnimationCurve heightCurve)
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        generateMesh();
+        generateMesh(xSize, ySize, scale, depth, heightCurve);
         updateMesh();
     }
 
@@ -41,25 +32,25 @@ public class ChunkGenerator : MonoBehaviour
     }
 
 
-    void generateMesh()
+    void generateMesh(int xSize, int ySize, float scale, float depth, AnimationCurve heightCurve)
     {
-        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
         int i = 0;
-        float[,] heightMap = GenerateHeightMap(xSize + 1, zSize + 1, xOffset, yOffset, octaves, persistance, lacunarity);
-        for (int z = 0; z <= zSize; z++)
+        //float[,] heightMap = GenerateHeightMap(xSize + 1, zSize + 1, xOffset, yOffset, octaves, persistance, lacunarity);
+        for (int z = 0; z <= ySize; z++)
         {
             for (int x = 0; x <= xSize; x++)
             {
-                float y = heightMap[x, z] * depth;
+                float y = heightCurve.Evaluate(heightMap[x, z]) * depth;
                 //print(y);
 
                 vertices[i++] = new Vector3(x * scale, y, z * scale);
             }
         }
-        triangles = new int[xSize * zSize * 6];
+        triangles = new int[xSize * ySize * 6];
         int vert = 0;
         int tris = 0;
-        for (int z = 0; z < zSize; z++)
+        for (int z = 0; z < ySize; z++)
         {
             for (int x = 0; x < xSize; x++)
             {
@@ -85,69 +76,4 @@ public class ChunkGenerator : MonoBehaviour
         mesh.RecalculateNormals();
     }
 
-    float calculateNoise(int x, int y, int octaves, float persistance, float lacunarity)
-    {
-        float noise = 0;
-        float amplitude = 1;
-        float frequency = 1;
-        float noiseHeight = 0;
-        float halfWidth = xSize / 2;
-        float halfHeight = zSize / 2;
-        for (int i = 0; i < octaves; i++)
-        {
-            float xCoord = ((float) (x - halfWidth) / resolution * frequency * scale) + xOffset;
-            float yCoord = ((float) (y - halfHeight) / resolution * frequency * scale) + yOffset;
-
-            noise = Mathf.PerlinNoise(xCoord, yCoord) * 2 - 1;
-
-            noiseHeight += noise * amplitude;
-
-            amplitude *= persistance;
-            frequency *= lacunarity;
-        }
-        noise *= noiseHeight;
-        return noise;
-    }
-
-    float[,] GenerateHeightMap(int xSize, int ySize, float xOffset, float yOffset, int octaves, float persistance, float lacunarity)
-    {
-        
-        float[,] heightMap = new float[xSize, ySize];
-        float maxHeightValue = float.MinValue;
-        float minHeightValue = float.MaxValue;
-        for (int x = 0; x < xSize; x++)
-        {
-            for (int y = 0; y < ySize; y++)
-            {
-                float amplitude = 1;
-                float frequency = 1;
-                float noiseHeight = 0;
-                for (int i = 0; i < octaves; i++)
-                {
-                    float xCoord = (((float)x + xOffset) /  frequency * scale);
-                    float yCoord = (((float)y + yOffset)/  frequency * scale);
-
-                    float noise = Mathf.PerlinNoise(xCoord, yCoord) * 2 - 1;
-                    //print(xCoord + " " + yCoord);
-
-                    noiseHeight += noise * amplitude;
-
-                    amplitude *= persistance;
-                    frequency *= lacunarity;
-                    //noise *= noiseHeight;
-                }
-                maxHeightValue = Mathf.Max(maxHeightValue, noiseHeight);
-                minHeightValue = Mathf.Min(minHeightValue, noiseHeight);
-                heightMap[x, y] = noiseHeight;
-            }
-        }
-        for (int x = 0; x < xSize; x++)
-        {
-            for (int y = 0; y < ySize; y++)
-            {
-                heightMap[x, y] = Mathf.InverseLerp(minHeightValue, maxHeightValue, heightMap[x, y]);
-            }
-        }
-        return heightMap;
-    }
 }
--- Procedural Generation/Assets/Scripts/WorldGenerator.cs
+++ Procedural Generation/Assets/Scripts/WorldGenerator.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class WorldGenerator : MonoBehaviour
 {
     public GameObject chunkObject;
-    private ChunkGenerator chunk;
     public int xSize = 16;
     public int zSize = 16;
 
-    public float xOffset;
-    public float yOffset;
-    public float step;
+    public Vector2 offset;
+    private float step;
 
-    public float scale = 0.25f;
+    public float chunkSize;
+    private float scale;
+    public float depth;
+    public AnimationCurve heightCurve;
 
+    public float noiseScale;
+    private float adjustedNoiseScale;
     public int chunkResolution;
     public int octaves;
     public float persistance;
@@ -23,57 +26,71 @@ public class WorldGenerator : MonoBehaviour
 
     public void Start()
     {
-        xOffset = Random.RandomRange(0, float.MaxValue);
-        yOffset = Random.RandomRange(0, float.MaxValue);
-        chunk = chunkObject.GetComponent<ChunkGenerator>();
         CreateWorld();
     }
 
-    void CreateWorld()
+    void DeleteWorld()
     {
+        Object[] allChunks = GameObject.FindObjectsOfType(typeof(ChunkGenerator));
+        foreach (ChunkGenerator chunk in allChunks)
+        {
+            DestroyImmediate(chunk.gameObject);
+        }
+    }
+
+    public void CreateWorld()
+    {
+        DeleteWorld();
+        scale = chunkSize * 2 / (float)chunkResolution;
+        adjustedNoiseScale = noiseScale / chunkResolution;
+        step = chunkSize * 2;
         for (int x = 0; x < xSize; x++)
         {
             for (int z = 0; z < zSize; z++)
             {
                 ChunkGenerator newChunk = Instantiate(chunkObject, new Vector3(x * step, 0, z * step), Quaternion.identity).GetComponent<ChunkGenerator>();
-                float[,] newHeightMap = GenerateHeightMap(chunkResolution + 1, chunkResolution + 1, xOffset + x * 32, yOffset + z * 32, octaves, persistance, lacunarity);
+                float[,] newHeightMap = GenerateHeightMap(chunkResolution + 1, chunkResolution + 1, (int)(Random.value * 10000), new Vector2(offset.x + x * chunkResolution, offset.y + z * chunkResolution), octaves, persistance, lacunarity);
                 newChunk.setHeightMap(newHeightMap);
-                newChunk.GenerateTerrain();
+                newChunk.GenerateTerrain(chunkResolution, chunkResolution, scale, depth, heightCurve);
 
             }
         }
     }
-    public float GenerateWorld(int x, int y)
-    {
-        xOffset = Random.RandomRange(0, 999999f);
-        yOffset = Random.RandomRange(0, 999999f);
 
-        float xCoord = (float)x / scale + xOffset;
-        float yCoord = (float)y / scale + yOffset;
-
-        float noise = Mathf.PerlinNoise(xCoord, yCoord);
-        return noise;
-    }
-    float[,] GenerateHeightMap(int xSize, int ySize, float xOffset, float yOffset, int octaves, float persistance, float lacunarity)
+    /*
+     * TODO: Make changes in mesh resolution not impact the position in the noise map
+     */
+    float[,] GenerateHeightMap(int xSize, int ySize, int seed, Vector2 offset, int octaves, float persistance, float lacunarity)
     {
+        System.Random prng = new System.Random(seed);
 
         float[,] heightMap = new float[xSize, ySize];
-        float maxHeightValue = float.MinValue;
-        float minHeightValue = float.MaxValue;
+        float maxPossibleHeight = 0;
+        float minPossibleHeight = 0;
+
+        float amplitude = 1;
+        float frequency = 1;
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i =  0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            maxPossibleHeight += amplitude;
+            amplitude *= persistance;
+        }
         for (int x = 0; x < xSize; x++)
         {
             for (int y = 0; y < ySize; y++)
             {
-                float amplitude = 1;
-                float frequency = 1;
+                amplitude = 1;
+                frequency = 1;
                 float noiseHeight = 0;
                 for (int i = 0; i < octaves; i++)
                 {
-                    float xCoord = (((float)x + xOffset) / frequency * scale);
-                    float yCoord = (((float)y + yOffset) / frequency * scale);
+                    float xCoord = (((float)x + offset.x) / frequency * adjustedNoiseScale);
+                    float yCoord = (((float)y + offset.y) / frequency * adjustedNoiseScale);
 
                     float noise = Mathf.PerlinNoise(xCoord, yCoord) * 2 - 1;
-                    print(((float)(x) + xOffset));
 
                     noiseHeight += noise * amplitude;
 
@@ -81,8 +98,6 @@ public class WorldGenerator : MonoBehaviour
                     frequency *= lacunarity;
                     //noise *= noiseHeight;
                 }
-                maxHeightValue = Mathf.Max(maxHeightValue, noiseHeight);
-                minHeightValue = Mathf.Min(minHeightValue, noiseHeight);
                 heightMap[x, y] = noiseHeight;
             }
         }
@@ -90,7 +105,8 @@ public class WorldGenerator : MonoBehaviour
         {
             for (int y = 0; y < ySize; y++)
             {
-                heightMap[x, y] = Mathf.InverseLerp(minHeightValue, maxHeightValue, heightMap[x, y]);
+                float normalizedHeight = (heightMap[x, y] + 1) / (2f * maxPossibleHeight / 1.75f);
+                heightMap[x, y] = normalizedHeight;
             }
         }
         return heightMap;
